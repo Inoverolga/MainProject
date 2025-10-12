85163556ba6dc0ce92c21cbd6fb70dc13f28c23d
Inover Olga <kuzma-inoverolga@mail.ru>
2025-10-12 17:14:11 +0300

    Update CORS with frontend URL

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://mainproject-front.onrender.com", "http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
