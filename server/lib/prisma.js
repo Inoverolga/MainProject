2a8ca18a29023fbd3fd07ca44444923ca2455fa0
Inover Olga <kuzma-inoverolga@mail.ru>
2025-10-14 16:25:04 +0300

    Add Prisma client

--- server/lib/prisma.js
+++ server/lib/prisma.js
@@ -1,3 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+// import pkg from "@prisma/client";
+// const { PrismaClient } = pkg;
 
-export const prisma = new PrismaClient();
+// export const prisma = new PrismaClient();
+
+export const prisma = {
+  $queryRaw: async (query) => {
+    console.log("Mock Prisma query:", query);
+    return [{ current_time: new Date().toISOString() }];
+  },
+};
