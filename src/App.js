b22e38c5339b82b37c649663c88281c9514f77d1
Inover Olga <kuzma-inoverolga@mail.ru>
2025-10-14 14:39:03 +0300

    fix: переход на HashRouter для SPA роутинга

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./page/mainpage/MainPage.js";
 import InventoryPage from "./page/inventorypage/InventoryPage.js";
 import RegistrationPage from "./page/registrationpage/RegistrationPage.js";
