a734b8618b2f0fe73790ca4f1421fc4b1a3e20b1
Inover Olga <kuzma-inoverolga@mail.ru>
2025-10-18 15:56:49 +0300

    готовая аутентификация

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import routerTag from "./routes/routerTags.js";
 import routerLogin from "./routes/routerLogin.js";
 import routerAuthO from "./routes/routerAuthO.js";
 import routerAuthMagic from "./routes/routerAuthMagic.js";
-import prisma from "./lib/prisma.js";
+import { prisma } from "./lib/prisma.js";
 
 const app = express();
 app.use(
