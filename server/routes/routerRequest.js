e108e92cd3047995f28ef6f142e28d15a920a033
Inover Olga <kuzma-inoverolga@mail.ru>
2025-11-19 03:29:23 +0300

    final

--- server/routes/routerRequest.js
+++ server/routes/routerRequest.js
@@ -23,7 +23,7 @@ routerRequest.post("/support", checkToken, async (req, res) => {
     console.log("=== SUPPORT REQUEST START ===");
     console.log("Body:", JSON.stringify(req.body, null, 2));
     console.log("User:", req.user);
-    const { problem, priority, inventoryId, pageUrl } = req.body;
+    const { problem, priority, pageUrl } = req.body;
     const userId = req.user.userId;
 
     if (!problem || !priority || !pageUrl) {
